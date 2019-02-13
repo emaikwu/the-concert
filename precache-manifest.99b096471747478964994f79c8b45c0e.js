053631ee5e70cac10fd2ada16ea731340fd688bb
Emaikwu Innocent <ennosaint@outlook.com>
2019-02-13 17:58:55 +0100

    Updates

---
+++ precache-manifest.99b096471747478964994f79c8b45c0e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9384277e1d86334dca41bf96d93fa70",
+    "url": "/legendary-concert/static/media/slide_two.d9384277.jpg"
+  },
+  {
+    "revision": "b527d3ba60fb36b98644f0c6a043d2b3",
+    "url": "/legendary-concert/static/media/slide_three.b527d3ba.jpg"
+  },
+  {
+    "revision": "d29529e0eee2267a18cea6d15715bf1a",
+    "url": "/legendary-concert/static/media/slide_one.d29529e0.jpg"
+  },
+  {
+    "revision": "a4358b67e0cd51a2a59d",
+    "url": "/legendary-concert/static/js/runtime~main.a4358b67.js"
+  },
+  {
+    "revision": "159e316339fa7cd59855",
+    "url": "/legendary-concert/static/js/main.159e3163.chunk.js"
+  },
+  {
+    "revision": "c69ccaa34301ad33fab5",
+    "url": "/legendary-concert/static/js/1.c69ccaa3.chunk.js"
+  },
+  {
+    "revision": "159e316339fa7cd59855",
+    "url": "/legendary-concert/static/css/main.aa302db2.chunk.css"
+  },
+  {
+    "revision": "68528a5f497e37978dadcd51e6aac2f9",
+    "url": "/legendary-concert/index.html"
+  }
+];
